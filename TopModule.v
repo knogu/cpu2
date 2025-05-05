85f1f6d9ee05063ea8b4a5fcc3d6640203930122
knogu <kotaronoguchi61@gmail.com>
2025-05-05 09:44:35 +0900

    display in fpga

--- TopModule.v
+++ TopModule.v
@@ -31,12 +31,24 @@ module TopModule(
         if (is_pc_updated) r_pc <= w_next_pc;
     end
 	
-    wire [7:0] inst_dec;
-    m_seven_segment seg1(inst_out[3:0], inst_dec);
-    assign HEX0 = inst_dec;
+    wire [7:0] imm_dec;
+    m_seven_segment seg1(ex.w_imm[3:0], imm_dec);
+    assign HEX0 = imm_dec;
+
+    wire [7:0] rs1_val;
+    m_seven_segment seg2(ex.w_rs1_val[3:0], rs1_val);
+    assign HEX1 = rs1_val;
+
+    wire [7:0] rs2_val;
+    m_seven_segment seg3(ex.w_rs2_val[3:0], rs2_val);
+    assign HEX2 = rs2_val;
+    
+    wire [7:0] alu_res_dec;
+    m_seven_segment seg4(ex.w_alu_res[3:0], alu_res_dec);
+    assign HEX3 = alu_res_dec;
 
     wire [7:0] pc_dec;
-    m_seven_segment seg2(r_pc[3:0], pc_dec);
-    assign HEX1 = pc_dec;
+    m_seven_segment seg5(r_pc[3:0], pc_dec);
+    assign HEX4 = pc_dec;
 	
 endmodule
