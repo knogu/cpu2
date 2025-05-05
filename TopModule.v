f6be3d7d6b37eca05e2319a0ef440c9470006954
knogu <kotaronoguchi61@gmail.com>
2025-05-05 21:40:58 +0900

    change fpga display

--- TopModule.v
+++ TopModule.v
@@ -23,38 +23,33 @@ module TopModule(
     m_prescale1000 u1(CLK1, c1, c2);
 
     reg [31:0] r_pc = 0;
-    wire [31:0] inst_out, w_next_pc;
-    wire[31:0] w_inst, w_rs1_val, w_rs2_val, w_alu_res;
-    wire[31:0] w_imm;
-    m_ex ex(c2, r_pc, w_next_pc, w_imm, w_rs1_val, w_rs2_val, w_alu_res, w_inst);
+    wire [31:0] w_next_pc;
+    wire[31:0] w_inst, w_alu_res, w_result, w_mem_out;
+    m_ex ex(c2, r_pc, w_next_pc, w_alu_res, w_result, w_inst, w_mem_out);
     reg is_pc_updated = 1;
     always @(posedge c2) begin
         is_pc_updated = ~is_pc_updated;
         if (is_pc_updated) r_pc <= w_next_pc;
     end
 	
-    wire [7:0] imm_dec;
-    m_seven_segment seg1(w_imm[3:0], imm_dec);
-    assign HEX0 = imm_dec;
-
-    wire [7:0] rs1_val;
-    m_seven_segment seg2(w_rs1_val[3:0], rs1_val);
-    assign HEX1 = rs1_val;
-
-    wire [7:0] rs2_val;
-    m_seven_segment seg3(w_rs2_val[3:0], rs2_val);
-    assign HEX2 = rs2_val;
-    
-    wire [7:0] alu_res_dec;
-    m_seven_segment seg4(w_alu_res[3:0], alu_res_dec);
-    assign HEX3 = alu_res_dec;
-
     wire [7:0] pc_dec;
-    m_seven_segment seg5(r_pc[3:0], pc_dec);
-    assign HEX4 = pc_dec;
+    m_seven_segment seg1(r_pc[3:0], pc_dec);
+    assign HEX0 = pc_dec;
 
     wire [7:0] inst_dec;
-    m_seven_segment seg6(w_inst[3:0], inst_dec);
-    assign HEX5 = inst_dec;
-	
+    m_seven_segment seg2(w_inst[3:0], inst_dec);
+    assign HEX1 = inst_dec;
+
+    wire [7:0] alu_res_dec;
+    m_seven_segment seg3(w_alu_res[3:0], alu_res_dec);
+    assign HEX2 = alu_res_dec;
+    
+    wire [7:0] mem_out_dec;
+    m_seven_segment seg4(w_mem_out[3:0], mem_out_dec);
+    assign HEX3 = mem_out_dec;
+
+    wire [7:0] result_dec;
+    m_seven_segment seg5(w_result[3:0], result_dec);
+    assign HEX4 = result_dec;
+
 endmodule
